a2d2177cffd21ce48e3ac05054db22a225eb0d34
Jen Speak <jspeak6@gmail.com>
2020-11-15 16:36:45 -0800

    finished app

---
+++ timer.js
@@ -0,0 +1,50 @@
+class Timer {
+    constructor(durationInput, startButton, pauseButton, callbacks) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+        if (callbacks) {
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
+
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+
+    start = () => {
+        if (this.onStart) {
+            this.onStart(this.timeRemaining);
+        }
+        this.tick();
+        // runs tick function every second but there is a 1 sec delay after clicking start so we have to call tick above
+        this.interval = setInterval(this.tick, 20);
+    };
+
+    pause = () => {
+        clearInterval(this.interval);
+    };
+
+    tick = () => {
+        if (this.timeRemaining <= 0) {
+            this.pause();
+            if (this.onComplete) {
+                this.onComplete();
+            }
+        } else {
+            this.timeRemaining = this.timeRemaining - 0.02;
+            if (this.onTick) {
+                this.onTick(this.timeRemaining);
+            }
+        }
+    };
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value);
+    }
+
+    set timeRemaining(time) {
+        this.durationInput.value = time.toFixed(2); //.toFixed() rounds the decimal in the time remaining to 2 decimal places
+    }
+}
